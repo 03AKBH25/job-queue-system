8486e2a8cf98bb23d525674d2c83b870ce585e74
Aniket Kumar <25aniket9@gmail.com>
2026-03-25 14:38:58 +0530

    added replay feature

--- api-server/src/controllers/jobController.js
+++ api-server/src/controllers/jobController.js
@@ -1,5 +1,7 @@
 import { createJobService } from "../services/jobServices.js"
 import { getJobByIdService } from "../services/jobServices.js"
+import prisma from "../../../shared/prismaClient.js"
+import jobQueue from "../queues/jobQueue.js"
 
 export const createJob = async (req, res) => {
   try {
@@ -47,6 +49,51 @@ export const getJobById = async(req, res)=>{
     return res.status(404).json({
       success: false,
       message: error.message
+    })
+  }
+}
+
+export const replayJob = async(req, res)=>{
+  const {jobId} = req.params
+
+  try{
+    //1. Fetch job from DB
+    const job = await prisma.job.findUnique({
+      where: {id: jobId}
+    })
+    if(!job){
+      return res.status(404).json({error: "Job not found"})
+    }
+
+    //2. Only allow replaying failed jobs
+    if(job.status !== "FAILED"){
+      return res.status(400).json({error: "Only failed jobs can be replayed"})
+    }
+
+    //3. Reset job in DB
+    await prisma.job.update({
+      where: {id: jobId},
+      data:{
+        status: "WAITING",
+        attempts: 0,
+        result: null,
+      }
+    })
+
+    //4. Add job back to queue
+    await jobQueue.add(job.type, {
+      jobId: job.id,
+      payload: job.payload,
+      type: job.type
+    })
+
+    return res.json({
+      message: "Job replayed successfully"
+    })
+  }catch(error){
+    console.error("Replay error:", error)
+    return res.status(500).json({
+      error: "Internal Server Error"
     })
   }
 }
